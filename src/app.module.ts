483e3a90033d0077bd9b11b77be06b380ac37bb3
nnutica <nitinat.loru@bumail.net>
2026-06-16 21:46:40 +0700

    Refactor Code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
-import { AiModule } from './ai/ai.module';
+import { AiModule } from './aimodule/ai.module';
 
 @Module({
   imports: [
